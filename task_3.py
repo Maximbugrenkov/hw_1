8aa1e3eb2886ed1e16bf809b2c7014c343ace40b
Максим С. Богренков <bogrenkov_ms@edu.kvantorium-pk.ru>
2020-03-02 11:16:04 +1000

    done

--- task_3.py
+++ task_3.py
@@ -6,9 +6,10 @@ from pyrob.api import *
 @task
 def task_3_1():
     if wall_is_on_the_right():
-    pass
-  else:
-      move_right
+        pass
+    else:
+        move_right()
+
 
 if __name__ == '__main__':
     run_tasks()
